3959e1391a6ea1de51508881946e89e149f8ecd3
foxsofter <foxsofter@foxsofterdeMacBook-Pro.local>
2016-12-04 11:54:52 +0800

    Initial commit

---
+++ Example/LPDCollectionViewKit/LPDViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPDViewController.h
+//  LPDCollectionViewKit
+//
+//  Created by foxsofter on 12/04/2016.
+//  Copyright (c) 2016 foxsofter. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LPDViewController : UIViewController
+
+@end
